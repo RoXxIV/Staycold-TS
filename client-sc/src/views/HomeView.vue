f122ace6da9307be210e16fecc3fbe2b9f6cf94e
evan <evan.hermier@gmail.com>
2023-10-16 19:19:35 +0200

    feat: Added global SCSS with variable definitions, font integration, and project structure cleanup

--- client-sc/src/views/HomeView.vue
+++ client-sc/src/views/HomeView.vue
@@ -1,9 +1,14 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Hello</h1>
+  <button>Click</button>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci culpa
+    dicta vero quae impedit, eveniet aliquid minus cum beatae magni modi illo
+    aliquam hic dolorum! Aliquam sequi fugiat similique magnam.
+  </p>
 </template>
+<style lang="scss">
+/* Your SCSS styles here */
+</style>
