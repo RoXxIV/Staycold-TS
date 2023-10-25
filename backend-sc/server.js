af5679be60ba4b53f24add87d90c4a8c6bdf8d8b
evan <evan.hermier@gmail.com>
2023-10-25 19:06:10 +0200

    feat: Implement Rate-Limiting and Basic Security Measures
    - Added rate-limiting to API routes to prevent abuse.
    - Implemented basic security measures using Helmet.

--- backend-sc/server.js
+++ backend-sc/server.js
@@ -5,12 +5,16 @@
  * @requires cors
  * @requires body-parser
  * @requires ./models
+ * @requires express-rate-limit
+ * @requires helmet
  */
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const db = require("./models");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 
 /**
  * Load environment variables from .env file
@@ -22,6 +26,24 @@ dotenv.config();
  * @type {express.Application}
  */
 const app = express();
+
+/**
+ * Use Helmet for basic security
+ */
+app.use(helmet());
+
+/**
+ * Rate-limiting configuration
+ */
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+});
+
+/**
+ * Apply rate-limiting to routes
+ */
+app.use("/api/", apiLimiter);
 
 /**
  * Middleware for enabling CORS
